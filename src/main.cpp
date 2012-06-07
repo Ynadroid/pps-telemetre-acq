190aed58a2cc742c5d21198122eb984f03754356
Ynadroid <danyberlemont@free.fr>
2012-06-07 20:46:44 +0200

    Développement des fonctions de lecture/écriture sur port série Il reste à interfacer les fonctions d'acquisition avec l'interface graphique De plus, il reste à développer l'algorithme exact de d'acquisition et de traitement des données entrantes sur le port série.

--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,46 @@
 #include <iostream>
-#include <time.h>
+#include <ctime>
 
-#include <Windows.h>
-#include <NIDAQmx.h>
+#include "Acquisitor.h"
 
-#define SAMPLE_SIZE 255
+static bool canAcq = false;
+
+class TestCallback : public AcquisitionCallback
+{
+   virtual void result(double distance)
+   {
+      std::cout << "Result is " << distance << std::endl;
+      canAcq = true;
+   }
+
+   virtual void error(std::string reason)
+   {
+      std::cerr << "Error: " << reason << std::endl;
+   }
+};
 
 int main(int argc,char** argv)
 {
 	std::cout << "Hello !" << std::endl;
 
-   // Task parameters
-   int32       error = 0;
-   TaskHandle  taskHandle = 0;
-   char        errBuff[2048]={'\0'};
+   Acquisitor acq;
+   TestCallback cb;
 
-   // Channel parameters
-   char        chan[] = "Dev1/ao0";
-   float64     min = 0.0;
-   float64     max = 5.0;
+   //acq.calibrate();
+   acq.launchAcquisition(ACQ_TYPE_ONE,cb);
+   int timer = 15;
+   time_t time = std::time(NULL);
+   while(!canAcq /*&& std::time(NULL) < time+timer*/) { Sleep(2000); }
 
-   // Timing parameters
-   uInt64      samplesPerChan = SAMPLE_SIZE;
+   unsigned int ressize = 0;
+   double* d = cb.getRawData(ressize);
 
-   // Data write parameters
-   float64     data[SAMPLE_SIZE];
-   int32       pointsWritten;
-   float64     timeout = 10.0;
-   float64     frequency = 5;
-   float64     amplitude = 5;
-
-   // Creating a task for the USB module
-	error = DAQmxCreateTask("MyTask",&taskHandle);
-   if( !DAQmxFailed(error) )
+   for(unsigned int i=0; i<ressize; i++)
    {
-      std::cout << "Frequency ?\n";
-      std::cin >> frequency;
-      std::cout << "timeout ?\n";
-      std::cin >> timeout;
-      std::cout << "Set: " << min << " " << max << " " << frequency << "\n";
-
-      // Setting output voltage signal
-      error = DAQmxCreateAOVoltageChan(taskHandle,chan,"",min,max,DAQmx_Val_Volts,NULL);
+      std::cout << d[i] << " ";
    }
-   else
-      std::cout << "Error CreateTask" << std::endl;
+   std::cout << std::endl;
 
-   // Do the task
-   if( !DAQmxFailed(error) )
-      error = DAQmxStartTask(taskHandle);
-   else
-      std::cout << "Error CreateVoltage" << std::endl;
-
-   double j=0;
-   for(int i=0;i<samplesPerChan;i++,j+=0.001) {
-      data[i] = abs(sin(j))*amplitude;
-   }
-
-   if( !DAQmxFailed(error) ) {
-      time_t t = time(NULL);
-      while(time(NULL) < t+timeout) {
-         error = DAQmxWriteAnalogF64(taskHandle,samplesPerChan,0,timeout,DAQmx_Val_GroupByChannel,data,&pointsWritten,NULL);
-      }
-   }
-   else
-      std::cout << "Error StartTask" << std::endl;
-
-   if( DAQmxFailed(error) )
-      DAQmxGetExtendedErrorInfo(errBuff,2048);
-
-   if( taskHandle!=0 )
-   {
-      DAQmxStopTask(taskHandle);
-      DAQmxClearTask(taskHandle);
-   }
-
-   if( DAQmxFailed(error) )
-   {
-	   std::cout << "DAQmxBase Error " << error << ": " << errBuff << "\n";
-   }
-
-   system("pause");
+   std::cout << "END" << std::endl;
    return 0;
 }
